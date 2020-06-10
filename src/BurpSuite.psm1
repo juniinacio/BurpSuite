5037b14bdb14a586d25ad846b03a96a85552a4de
Juni Inacio <Juni.Inacio@macaw.nl>
2020-06-10 21:32:27 +0200

    Disable weak protocols by default

--- src/BurpSuite.psm1
+++ src/BurpSuite.psm1
@@ -1,2 +1,3 @@
 using namespace System
 
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12 -bor [Net.SecurityProtocolType]::Tls13
