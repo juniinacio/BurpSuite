c4f975eb262e8076e58c1d7132d05566032c7f0c
juni inacio <juni.inacio@ssw.shell.com>
2020-06-12 14:37:58 +0200

    Better reading

--- src/Public/Get-BurpSuiteAgent.ps1
+++ src/Public/Get-BurpSuiteAgent.ps1
@@ -29,16 +29,20 @@ function Get-BurpSuiteAgent {
 
         $agentFields = [Query]::New('agent')
         $PSBoundParameters['Fields'] | ForEach-Object { $agentFields.AddField($_) | Out-Null }
+
         if ($PSBoundParameters.ContainsKey('ID')) { $agentFields.AddArgument('id', '$id') | Out-Null }
 
         if ($PSBoundParameters.ContainsKey('ErrorFields')) {
             $errorField = [Query]::New('error')
+
             $PSBoundParameters['ErrorFields'] | ForEach-Object { $errorField.AddField($_) | Out-Null }
+
             $agentFields.AddField($errorField) | Out-Null
         }
 
         $agentQuery = [Query]::New($operationName)
         $agentQuery.AddField($agentFields) | Out-Null
+
         if ($PSBoundParameters.ContainsKey('ID')) { $agentQuery.AddArgument('$id', 'ID!') | Out-Null }
 
         $query = 'query {0}' -f $agentQuery
