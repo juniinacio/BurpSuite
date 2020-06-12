56bc89043e98a03636e6f6ad7ac85f9566ec279f
juni inacio <juni.inacio@ssw.shell.com>
2020-06-12 09:03:16 +0200

    Add operation name property

--- src/Classes/GraphRequest.ps1
+++ src/Classes/GraphRequest.ps1
@@ -1,13 +1,25 @@
 class GraphRequest {
     hidden [string] $Query = [string]::Empty
+    hidden [string] $OperationName = [string]::Empty
     hidden [hashtable] $Variables = @{}
 
     GraphRequest([string] $query) {
         $this.Query = $Query
     }
 
+    GraphRequest([string] $query, [string] $operationName) {
+        $this.Query = $Query
+        $this.OperationName = $operationName
+    }
+
     GraphRequest([string] $query, [hashtable] $variables) {
         $this.Query = $Query
+        $this.Variables = $Variables
+    }
+
+    GraphRequest([string] $query, [string] $operationName, [hashtable] $variables) {
+        $this.Query = $Query
+        $this.OperationName = $operationName
         $this.Variables = $Variables
     }
 }
--- unit/tests/classes/GraphRequest.tests.ps1
+++ unit/tests/classes/GraphRequest.tests.ps1
@@ -12,16 +12,44 @@ InModuleScope $env:BHProjectName {
                 $assert.Query | Should -Be $query
             }
 
+            It "constructor should set both query and operation name property" {
+                # arrange
+                $query = '{ __schema { queryType { name } } }'
+                $operationName = '__schema'
+
+                # act
+                $assert = [GraphRequest]::new($query, $operationName)
+
+                # assert
+                $assert.Query | Should -Be $query
+                $assert.OperationName | Should -Be '__schema'
+            }
+
             It "constructor should set both query and variables property" {
                 # arrange
                 $query = '{ __schema { queryType { name } } }'
-                $variables = @{Foo = 'Bar'}
+                $variables = @{Foo = 'Bar' }
 
                 # act
                 $assert = [GraphRequest]::new($query, $variables)
 
                 # assert
                 $assert.Query | Should -Be $query
+                $assert.Variables.Foo | Should -Be 'Bar'
+            }
+
+            It "constructor should set both query, operation name and variables property" {
+                # arrange
+                $query = '{ __schema { queryType { name } } }'
+                $operationName = '__schema'
+                $variables = @{Foo = 'Bar' }
+
+                # act
+                $assert = [GraphRequest]::new($query, $operationName, $variables)
+
+                # assert
+                $assert.Query | Should -Be $query
+                $assert.OperationName | Should -Be '__schema'
                 $assert.Variables.Foo | Should -Be 'Bar'
             }
         }
