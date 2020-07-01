dd24fa8c31c955be7b551c1f7b807ba198dbea9e
Juni Inacio <24542013+juniinacio@users.noreply.github.com>
2020-07-01 11:24:40 +0200

    Label and Credential params should be optional

--- unit/tests/public/Update-BurpSuiteSiteApplicationLogin.tests.ps1
+++ unit/tests/public/Update-BurpSuiteSiteApplicationLogin.tests.ps1
@@ -29,5 +29,67 @@ InModuleScope $env:BHProjectName {
                     -and $Request.Variables.Input.password -eq "changeme"
             }
         }
+
+        Context "Label" {
+            It "should update site application login label" {
+                # arrange
+                Mock -CommandName _callAPI -MockWith {
+                    [PSCustomObject]@{
+                        data = [PSCustomObject]@{
+                            update_site_application_login = [PSCustomObject]@{
+                                application_login = [PSCustomObject]@{
+                                    id = 1
+                                }
+                            }
+                        }
+                    }
+                }
+
+                # act
+                Update-BurpSuiteSiteApplicationLogin -Id 42 -Label "admin"
+
+                # assert
+                Should -Invoke _callAPI -ParameterFilter {
+                    $Request.OperationName -eq "UpdateSiteApplicationLogin" `
+                        -and $Request.Query -eq 'mutation UpdateSiteApplicationLogin($input:''UpdateSiteApplicationLoginInput!'') { update_site_application_login(input:''$input'') { application_login { id label username } } }' `
+                        -and $Request.Variables.Input.id -eq 42 `
+                        -and $Request.Variables.Input.label -eq "admin" `
+                        -and $Request.Variables.Input.ContainsKey('username') -eq $false `
+                        -and $Request.Variables.Input.ContainsKey('password') -eq $false
+                }
+            }
+        }
+
+        Context "Credential" {
+            It "should update site application login credential" {
+                # arrange
+                Mock -CommandName _callAPI -MockWith {
+                    [PSCustomObject]@{
+                        data = [PSCustomObject]@{
+                            update_site_application_login = [PSCustomObject]@{
+                                application_login = [PSCustomObject]@{
+                                    id = 1
+                                }
+                            }
+                        }
+                    }
+                }
+
+                $credentials = New-Object System.Management.Automation.PSCredential ("administrator", $(ConvertTo-SecureString "changeme" -AsPlainText -Force))
+
+                # act
+                Update-BurpSuiteSiteApplicationLogin -Id 42 -Credential $credentials
+
+                # assert
+                Should -Invoke _callAPI -ParameterFilter {
+                    $Request.OperationName -eq "UpdateSiteApplicationLogin" `
+                        -and $Request.Query -eq 'mutation UpdateSiteApplicationLogin($input:''UpdateSiteApplicationLoginInput!'') { update_site_application_login(input:''$input'') { application_login { id label username } } }' `
+                        -and $Request.Variables.Input.id -eq 42 `
+                        -and $Request.Variables.Input.ContainsKey('label') -eq $false `
+                        -and $Request.Variables.Input.username -eq "administrator" `
+                        -and $Request.Variables.Input.password -eq "changeme"
+                }
+            }
+        }
     }
 }
